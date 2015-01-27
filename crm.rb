0e2c4828b9ffcd5ea83f220ccbae0a829085bbff
Brant <boshartb@gmail.com>
2015-01-27 15:39:25 -0500

    contacts do

--- crm.rb
+++ crm.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require "sinatra/reloader"
 
 get '/' do
 	@crm_app_name = "My CRM"
   erb :index
+end
+
+get "/contacts/new" do
+	erb :contacts
+end
+
+get "/contacts" do
+	erb :contacts
 end
