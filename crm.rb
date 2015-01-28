3da11e2caca58dde4bec63e9a6e6ae68e7581b19
Brant <boshartb@gmail.com>
2015-01-28 16:16:11 -0500

    Crm fixed up and layout.erb added

--- crm.rb
+++ crm.rb
@@ -3,33 +3,27 @@ require_relative 'contact'
 require 'sinatra'
 require "sinatra/reloader"
 
-$rolodex= Rolodex.new
+$rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer"))
+
 
 get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
 
-get "/contacts/new" do
-  erb :contacts
-end
-
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+  @contacts = $rolodex.contacts
 
   erb :contacts
-end
-
-post '/contacts' do
-  puts params
 end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
-  post '/contacts' do
-  end
+  redirect to('/contacts')
+end
+
+get '/contacts/new' do
+  erb :new_contact
 end
