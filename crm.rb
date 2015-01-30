2cc3def5a7baf0a58702d9ecf50c4c434f769256
Brant <boshartb@gmail.com>
2015-01-30 11:26:57 -0500

    datamapper info started

--- crm.rb
+++ crm.rb
@@ -1,11 +1,32 @@
 require_relative 'rolodex'
-require_relative 'contact'
 require 'sinatra'
 require "sinatra/reloader"
 require 'pry'
+require "data_mapper"
 
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+  include DataMapper::Resource
+
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @id = nil
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+end
+
+
+
+# end of datamapper setup
 $rolodex = Rolodex.new
 
+
+#begin sinatra routes
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -18,14 +39,15 @@ get "/contacts" do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :new_contact
+end
+
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
-end
-
-get '/contacts/new' do
-  erb :new_contact
 end
 
 
